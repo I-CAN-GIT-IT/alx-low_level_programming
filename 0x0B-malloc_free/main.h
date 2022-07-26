d4ec986b27a1159f02372131ca27b11c25d2e832
I-CAN-GIT-IT <jameskm6412@gmail.com>
2022-07-26 07:31:47 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,8 @@
+int _putchar(char);
+char *create_array(unsigned int, char);
+char *_strdup(char *);
+char *str_concat(char *, char *);
+int **alloc_grid(int, int);
+void free_grid(int **, int);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
